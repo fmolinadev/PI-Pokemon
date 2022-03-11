5fa245e7a6f2519616efaf7fc9f5a75285a9b927
The Francisco Molina <fdm.molina@gmail.com>
2022-03-11 19:26:35 -0300

    Pokemon add

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,45 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, UUIDV4 } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
+  sequelize.define("pokemon", {
+    id: {
+      type: DataTypes.UUID,
+      DefaultValue: UUIDV4,
+      allowNull: false,
+      unique: true,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
+    },
+    image: {
+      type: DataTypes.TEXT,
+      validate: { isUrl: true },
+      allowNull: true,
+      // defaultValue: "https://i.imgur.com/Saga2pd.jpg?1",
+    },
+    life: {
+      type: DataTypes.INTEGER,
+    },
+    force: {
+      type: DataTypes.INTEGER,
+    },
+
+    defending: {
+      type: DataTypes.INTEGER,
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+    },
+    height: {
+      type: DataTypes.FLOAT,
+    },
+    weight: {
+      type: DataTypes.FLOAT,
     },
   });
 };
