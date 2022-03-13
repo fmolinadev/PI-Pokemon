b1380758179c12c7bcd81edeb6b30e3a6766ffe3
The Francisco Molina <fdm.molina@gmail.com>
2022-03-12 21:11:41 -0300

    Models verificados

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -3,7 +3,7 @@ const { DataTypes, UUIDV4 } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("pokemon", {
+  sequelize.define("Pokemon", {
     id: {
       type: DataTypes.UUID,
       DefaultValue: UUIDV4,
@@ -24,26 +24,50 @@ module.exports = (sequelize) => {
     },
     life: {
       type: DataTypes.INTEGER,
+      validate: {
+        min: 1,
+        max: 100,
+      },
     },
     attack: {
       type: DataTypes.INTEGER,
+      validate: {
+        min: 1,
+        max: 100,
+      },
     },
     defense: {
       type: DataTypes.INTEGER,
+      validate: {
+        min: 1,
+        max: 100,
+      },
     },
     speed: {
       type: DataTypes.INTEGER,
+      validate: {
+        min: 1,
+        max: 100,
+      },
     },
     height: {
       type: DataTypes.INTEGER,
+      validate: {
+        min: 1,
+        max: 50, //El pokemon más alto es Eternatus (20m)
+      },
     },
     weight: {
       type: DataTypes.INTEGER,
+      validate: {
+        min: 1,
+        max: 1000, //El pokemon más pesado es Cosmoem con 999kg.
+      },
     },
     createdPokemon: {
       type: DataTypes.BOOLEAN,
-      allowNull: false,
       defaultValue: true,
+      allowNull: false,
     },
   });
 };
--- api/src/models/Types.js
+++ api/src/models/Types.js
@@ -3,10 +3,16 @@ const { DataTypes } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("type", {
+  sequelize.define("Types", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+      unique: true,
+    },
     name: {
       type: DataTypes.STRING,
-      allowNull: false,
     },
   });
 };
