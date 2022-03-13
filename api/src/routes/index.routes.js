2dc5576a682ebe67ca52c8177ddd9ddfd9332ccb
The Francisco Molina <fdm.molina@gmail.com>
2022-03-12 21:14:48 -0300

    Index routes ready

--- api/src/routes/index.routes.js
+++ api/src/routes/index.routes.js
@@ -1,12 +1,13 @@
-const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+// Configurar los routers
+const { Router } = require("express");
+const pokemonRoute = require("./pokemon.routes");
+const typesRoute = require("./types.routes");
 
 const router = Router();
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
-
+router.use("/pokemon", pokemonRoute);
+router.use("/types", typesRoute);
 
 module.exports = router;
