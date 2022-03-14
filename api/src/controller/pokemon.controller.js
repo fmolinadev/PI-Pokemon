8875b3dcb06c19509356760a9ea95abcd8373119
The Francisco Molina <fdm.molina@gmail.com>
2022-03-13 22:47:55 -0300

    Controllers para revisar

--- api/src/controller/pokemon.controller.js
+++ api/src/controller/pokemon.controller.js
@@ -1,8 +1,12 @@
 const { allPokemon, allPokeId } = require("../utils/pokemonUtils");
+const { Types } = require("../db");
+
+////////////////////GET POR NOMBRE O TODOS LOS POKEMONES///////////////////////
 
 const getPokemon = async (req, res) => {
-  const { name } = req.query;
+  const { name } = req.query; //Si recibo name, lo busco.
   try {
+    //Lo busco en todos los pokemones concatenados desde mi UTILS. Se de donde viene por la propiedad "createPokemon".
     let infoPokemon = await allPokemon();
     if (name) {
       let pokemonName = await infoPokemon.filter(
@@ -14,12 +18,15 @@ const getPokemon = async (req, res) => {
         res.status(200).send(pokemonName);
       }
     } else {
+      //Si no recibo nombre, devuelvo todos los pokemones.
       res.status(200).json(infoPokemon);
     }
   } catch (error) {
-    next(error);
+    res.status(404).send(error);
   }
 };
+
+/////////////////////////////GET X ID ///////////////////////////////7///
 
 //Si el GET es de un personaje por el ID que lo recibo por Params o Query:
 const getByID = async (req, res) => {
@@ -31,18 +38,31 @@ const getByID = async (req, res) => {
     res.status(404).send(error);
   }
 };
+//////////////////////////////POST/////////////////////////////////////
 
-const createPokemon = async (req, res) => {
-  const { name, life, image, attack, defense, speed, height, weight, types } =
-    req.body;
-
+const postPokemon = async (req, res) => {
   try {
+    const { name, life, image, attack, defense, speed, height, weight, types } =
+      req.body;
+    //Valido todo:
+    if (!Object.keys(req.body).length)
+      return res
+        .status(400)
+        .send({ msg: "Error. No se recibio información para agregar" });
+    //Valido solamente name(es required en mi DB)
+    if (!name) {
+      return res
+        .status(404)
+        .json({ msg: "Error. No se inserto el nombre del Pokemon." });
+    }
+    //Podia usar el findOrCreate pero lo separo para manerar el error por separado.
+
     let findOnePokemon = await Pokemon.findOne({
       where: {
         name: name.toLowerCase(),
       },
     });
-
+    //Primero verifico que el nombre este disponible.
     if (findOnePokemon)
       return res.json({ msg: "El Pokemon ya existe. Intenta crear otro." });
 
@@ -56,22 +76,22 @@ const createPokemon = async (req, res) => {
       height: height,
       weight: weight,
     });
-
-    let pokemonType = await Type.findAll({
+    //Tipo de PK:
+    let pokemonTypeBD = await Types.findAll({
       where: {
         name: types,
       },
     });
-
-    await newPokemon.addType(pokemonType);
-    res.status(200).json({ msg: "Pokemon creado con éxito." });
+    //Le paso el Types:
+    newPokemon?.addTypes(pokemonTypeBD);
+    res.status(201).json({ msg: "Pokemon creado con éxito." });
   } catch (error) {
     res.status(404).send(error);
+    console.log(error);
   }
 };
-
 module.exports = {
   getPokemon,
   getByID,
-  createPokemon,
+  postPokemon,
 };
--- api/src/controller/types.controller.js
+++ api/src/controller/types.controller.js
@@ -1,11 +1,27 @@
-const { getTypes } = require("../utils/typesUtils");
+const axios = require("axios");
+const { Types } = require("../db.js");
+const URLTYPE = "https://pokeapi.co/api/v2/type";
+
+/////////////////////GET TYPES ///////////////////////////////////
 
 const getAllTypes = async (req, res) => {
   try {
-    let pokemonType = await getTypes();
-    res.json(pokemonType);
+    const getUrlType = await axios.get(URLTYPE);
+    //Llamado a la ruta Types de la API.
+    allTypes = getUrlType.data.results;
+
+    allTypes.forEach((e) => {
+      Types.findOrCreate({
+        //Busca el type y si no encuentra, lo crea.
+        where: { name: e.name },
+      });
+    });
+
+    //Traigo el resto de los Types
+    const allPokemonDbTypes = await Types.findAll();
+    res.status(200).send(allPokemonDbTypes);
   } catch (error) {
-    res.status(404).send(error);
+    res.status(404).send({ error: "Ouch! We have an error server." });
   }
 };
 module.exports = getAllTypes;
