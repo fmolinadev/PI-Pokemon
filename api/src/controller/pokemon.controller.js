37db739d229e7bf9e908483a69bd3d90f39b198f
The Francisco Molina <fdm.molina@gmail.com>
2022-03-14 23:31:23 -0300

    Controllers fixed

--- api/src/controller/pokemon.controller.js
+++ api/src/controller/pokemon.controller.js
@@ -1,11 +1,11 @@
-const { allPokemon, allPokeId } = require("../utils/pokemonUtils");
-const { Types } = require("../db");
+const { allPokemon, allPokeId } = require("../utils/utils");
+const { Pokemon, Types } = require("../db");
 
 ////////////////////GET POR NOMBRE O TODOS LOS POKEMONES///////////////////////
 
 const getPokemon = async (req, res) => {
-  const { name } = req.query; //Si recibo name, lo busco.
   try {
+    const { name } = req.query; //Si recibo name, lo busco.
     //Lo busco en todos los pokemones concatenados desde mi UTILS. Se de donde viene por la propiedad "createPokemon".
     let infoPokemon = await allPokemon();
     if (name) {
@@ -15,21 +15,20 @@ const getPokemon = async (req, res) => {
       if (pokemonName.length === 0) {
         res.status(404).send("No se encontro ningun Pokemon con ese nombre");
       } else {
-        res.status(200).send(pokemonName);
+        return res.status(200).json(pokemonName);
       }
     } else {
       //Si no recibo nombre, devuelvo todos los pokemones.
-      res.status(200).json(infoPokemon);
+      return res.status(200).json(infoPokemon);
     }
   } catch (error) {
     res.status(404).send(error);
   }
 };
-
 /////////////////////////////GET X ID ///////////////////////////////7///
 
 //Si el GET es de un personaje por el ID que lo recibo por Params o Query:
-const getByID = async (req, res) => {
+async function getByID(req, res) {
   const { id } = req.params;
   try {
     let infoPokemon = await allPokeId(id);
@@ -37,26 +36,25 @@ const getByID = async (req, res) => {
   } catch (error) {
     res.status(404).send(error);
   }
-};
+}
 //////////////////////////////POST/////////////////////////////////////
 
 const postPokemon = async (req, res) => {
+  const { name, life, image, attack, defense, speed, height, weight, types } =
+    req.body;
+  //Valido todo:
+  if (!Object.keys(req.body).length)
+    return res
+      .status(400)
+      .send({ msg: "Error. No se recibio información para agregar" });
+  //Valido solamente name(es required en mi DB)
+  if (!name) {
+    return res
+      .status(404)
+      .send({ msg: "Error. No se inserto el nombre del Pokemon." });
+  }
+  //Podia usar el findOrCreate pero lo separo para manerar el error por separado.
   try {
-    const { name, life, image, attack, defense, speed, height, weight, types } =
-      req.body;
-    //Valido todo:
-    if (!Object.keys(req.body).length)
-      return res
-        .status(400)
-        .send({ msg: "Error. No se recibio información para agregar" });
-    //Valido solamente name(es required en mi DB)
-    if (!name) {
-      return res
-        .status(404)
-        .json({ msg: "Error. No se inserto el nombre del Pokemon." });
-    }
-    //Podia usar el findOrCreate pero lo separo para manerar el error por separado.
-
     let findOnePokemon = await Pokemon.findOne({
       where: {
         name: name.toLowerCase(),
@@ -67,7 +65,7 @@ const postPokemon = async (req, res) => {
       return res.json({ msg: "El Pokemon ya existe. Intenta crear otro." });
 
     let newPokemon = await Pokemon.create({
-      name: name,
+      name: name.toLowerCase(),
       image: image,
       life: life,
       attack: attack,
@@ -75,6 +73,7 @@ const postPokemon = async (req, res) => {
       speed: speed,
       height: height,
       weight: weight,
+      types,
     });
     //Tipo de PK:
     let pokemonTypeBD = await Types.findAll({
@@ -83,8 +82,10 @@ const postPokemon = async (req, res) => {
       },
     });
     //Le paso el Types:
-    newPokemon?.addTypes(pokemonTypeBD);
-    res.status(201).json({ msg: "Pokemon creado con éxito." });
+    await newPokemon.setTypes(pokemonTypeBD);
+    return res
+      .status(201)
+      .send({ msg: `El Pokemon ${newPokemon.name} fue creado con éxito!.` });
   } catch (error) {
     res.status(404).send(error);
     console.log(error);
