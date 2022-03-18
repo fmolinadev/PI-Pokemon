46f02f92392c63ad644b6574ab59c1e34dd8d282
The Francisco Molina <fdm.molina@gmail.com>
2022-03-17 23:57:35 -0300

    Models Types OK

--- api/src/models/Types.js
+++ api/src/models/Types.js
@@ -3,17 +3,23 @@ const { DataTypes } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("Types", {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey: true,
-      unique: true,
+  sequelize.define(
+    "Types",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+        unique: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        defaultValue: "This data was not found.",
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      defaultValue: "This data was not found.",
-    },
-  });
+    { timestamps: false }
+  );
+  {
+  }
 };
