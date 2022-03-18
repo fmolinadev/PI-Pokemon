c3fc16c8ff17987628f569e7d98cf10281f9bd61
The Francisco Molina <fdm.molina@gmail.com>
2022-03-17 23:58:09 -0300

    Index OK

--- api/src/routes/index.routes.js
+++ api/src/routes/index.routes.js
@@ -2,10 +2,9 @@
 // Ejemplo: const authRouter = require('./auth.js');
 // Configurar los routers
 const { Router } = require("express");
+const router = Router();
 const pokemonRoute = require("./pokemon.routes");
 const typesRoute = require("./types.routes");
-
-const router = Router();
 
 router.use("/pokemon", pokemonRoute);
 router.use("/types", typesRoute);
