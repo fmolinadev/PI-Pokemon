7871509288003bf7d54b92dbdba79342681d8960
The Francisco Molina <fdm.molina@gmail.com>
2022-03-19 12:07:05 -0300

    Routes OK. Revisar GET a names

--- api/src/routes/pokemon.routes.js
+++ api/src/routes/pokemon.routes.js
@@ -1,13 +1,14 @@
 //De todas las funcionalidades del controlador del cual se encarga index.
 const { Router } = require("express");
 const router = Router();
-
 const {
   getPokemon,
   getID,
   postPokemon,
 } = require("../controller/pokemon.controller");
+
 router.get("/", getPokemon);
+router.get("/?name=", getPokemon);
 router.get("/:id", getID);
 router.post("/create", postPokemon);
 module.exports = router;
