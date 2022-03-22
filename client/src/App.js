e0ff853ba8955abe4e148ddb8ae67ded69a8d788
The Francisco Molina <fdm.molina@gmail.com>
2022-03-22 15:39:43 -0300

    Routes OK

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { LandingPage } from "./components/LandingPage/LandingPage.jsx";
+import { Home } from "./components/HomePage/HomePage.jsx";
+import { Create } from "./components/Create/Create.jsx";
+import { Details } from "./components/Details/Details.jsx";
+import { ErrorPage } from "./components/errorPage/ErrorPage";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Pokemon</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="/pokemon" element={<Home />} />
+        <Route exact path="/pokemon/:id" element={<Details />} />
+        <Route exact path="/create" element={<Create />} />
+        {/* <Route exact path="/about" element={<AbautMe />} /> */}
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
