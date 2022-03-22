6959643d05ba42bf9977fa6b743042b088f1f78b
The Francisco Molina <fdm.molina@gmail.com>
2022-03-22 17:20:44 -0300

    Landing OK

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./LandingStyles.css";
 import imgLanding from "../../assets/IMAGE_LANDING_PNG.png";
 
 export function LandingPage() {
   return (
-    <div>
-      <section>
-        <h1>"¿Quieres saber todo sobre tu Pokemon favorito?"</h1>
+    <div class="landing-container">
+      <section class="content">
+        <h1 class="title">¿Quieres conocer más sobre tu Pokémon favorito?</h1>
         <Link to="/pokemon">
-          <button>¡Hazlo ahora!</button>
+          <button class="btn">¡Hazlo ahora!</button>
         </Link>
       </section>
       <Link to="/pokemon">
-        <img src={imgLanding} alt="dogs landing" />
+        <img class="img" src={imgLanding} alt="dogs landing" />
       </Link>
     </div>
   );
