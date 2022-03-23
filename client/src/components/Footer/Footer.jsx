e42d79376a348f9781445ac536fa9fd06a566ab8
The Francisco Molina <fdm.molina@gmail.com>
2022-03-22 23:08:05 -0300

    Footer OK

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import "./FooterStyles.css";
 
-export function FooterPage() {
+export function Footer() {
   return (
-    <footer>
-      <span>© Francisco Molina | PI Pokemon | Henry Bootcamp</span>
+    <footer class="footer-container">
+      <span class="footer-span">
+        © Francisco Molina | PI Pokemon | Henry Bootcamp
+      </span>
     </footer>
   );
 }
