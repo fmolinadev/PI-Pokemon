8db5120878572e2ac3e7187f29c99852874daf08
The Francisco Molina <fdm.molina@gmail.com>
2022-03-24 17:40:03 -0300

    Searchbar OK

---
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { getPokemonName } from "../../actions/pokemon.actions";
+import "./SearchStyles.css";
+
+export function SearchBar() {
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+
+  //Handle del input
+  function handleInputChange(e) {
+    e.preventDefault();
+    setName(e.target.value);
+  }
+  //Handle del serach
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch(getPokemonName(name));
+  }
+
+  return (
+    <div class="search-container">
+      <input
+        class="input-search"
+        onChange={(e) => handleInputChange(e)}
+        type="text"
+        placeholder="¿Cómo se llama el Pokémon?"
+      />
+      <button class="btn-search" onClick={(e) => handleSubmit(e)} type="submit">
+        Buscar
+      </button>
+    </div>
+  );
+}
