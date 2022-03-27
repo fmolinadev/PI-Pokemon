bc70e8051cbdf8ae4042be5533bc259cec95f42b
The Francisco Molina <fdm.molina@gmail.com>
2022-03-27 18:15:12 -0300

    Form para debug

---
+++ client/src/components/Create/Create.jsx
@@ -0,0 +1,261 @@
+import React from "react";
+import { useState, useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+import { NavBar } from "../Nav/NavBar";
+import { Footer } from "../Footer/Footer";
+import {
+  postPokemon,
+  getTypes,
+  getAllPokemon,
+} from "../../actions/pokemon.actions";
+import { useDispatch, useSelector } from "react-redux";
+import "./CreateStyles.css";
+
+//Validacion del formulario:
+import { validate } from "./validateForm";
+
+export function Create() {
+  const dispatch = useDispatch();
+  //   const navigate = useNavigate();
+  const stateTypes = useSelector((state) => state.types);
+  const totalPokemon = useSelector((state) => state.pokemons);
+
+  const [errors, setErrorForm] = useState({});
+  const [input, setInput] = useState({
+    name: ``,
+    life: ``,
+    attack: ``,
+    defense: ``,
+    speed: ``,
+    height: ``,
+    weight: ``,
+    typeA: ``,
+    typeB: ``,
+  });
+
+  useEffect(() => {
+    dispatch(getTypes());
+    dispatch(getAllPokemon());
+    // console.log(getTypes)
+  }, [dispatch]);
+
+  const handleInputChange = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+    setErrorForm(validate({ ...input, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let findName = totalPokemon.filter(
+        (e) => e.name.toLowerCase() === input.name.toLowerCase()
+      );
+
+      if (!findName)
+        return alert("Ya existe un pokemon con este nombre. ¡Cambialo!");
+      else {
+        const newPokemon = {
+          name: input.name,
+          life: input.life,
+          attack: input.attack,
+          defense: input.defense,
+          speed: input.speed,
+          height: input.height,
+          weight: input.weight,
+          typeA: input.typeA,
+          typeB: input.typeB,
+        };
+        dispatch(postPokemon(newPokemon));
+      }
+
+      setInput({
+        name: ``,
+        life: ``,
+        attack: ``,
+        defense: ``,
+        speed: ``,
+        height: ``,
+        weight: ``,
+        typeA: ``,
+        typeB: ``,
+      });
+      return alert(`El Pokémon fue creado con éxito.`);
+      //   navigate("/pokemon");
+    } catch (error) {
+      console.log(error);
+      return alert(
+        "Oh no! Algo falló al crear el Pokémon. ¡Intentalo de nuevo!"
+      );
+    }
+  };
+
+  return (
+    <div class="create_container">
+      <header>
+        <NavBar />
+      </header>
+      <h1 class="title">¡Crea un nuevo Pokémon!</h1>
+      <form class="form" onSubmit={handleSubmit}>
+        <div class="info-form">
+          <label>
+            Nombre
+            <input
+              onChange={handleInputChange}
+              value={input.name}
+              name="name"
+              type="text"
+              placeholder="¿Cómo se llama el Pokémon?..."
+              required
+            />
+            {errors.name && (
+              <div class="errors">
+                <div id="name">{errors.name}</div>
+              </div>
+            )}
+          </label>
+          <label>
+            Vida
+            <input
+              onChange={handleInputChange}
+              value={input.life}
+              name="life"
+              type="number"
+              min="1"
+              placeholder="Vida..."
+            />
+            {errors.life && (
+              <div class="errors">
+                <div>{errors.life}</div>
+              </div>
+            )}
+          </label>
+          <label>
+            Ataque
+            <input
+              onChange={handleInputChange}
+              value={input.attack}
+              name="attack"
+              type="number"
+              min="1"
+              placeholder="Poder de ataque..."
+            />
+            {errors.attack && (
+              <div class="errors">
+                <div id="attack">{errors.attack}</div>
+              </div>
+            )}
+          </label>
+          <label>
+            Defensa
+            <input
+              onChange={handleInputChange}
+              value={input.defense}
+              name="defense"
+              type="number"
+              min="1"
+              placeholder="Poder de defensa.."
+            />
+            {errors.defense && (
+              <div class="errors">
+                <div id="defense">{errors.defense}</div>
+              </div>
+            )}
+          </label>
+          <label>
+            Velocidad
+            <input
+              onChange={handleInputChange}
+              value={input.speed}
+              name="speed"
+              type="number"
+              min="1"
+              placeholder="Inserta su velocidad..."
+            />
+            {errors.speed && (
+              <div class="errors">
+                <div>{errors.speed}</div>
+              </div>
+            )}
+          </label>
+          <label>
+            Peso
+            <input
+              onChange={handleInputChange}
+              value={input.weight}
+              name="weight"
+              type="number"
+              min="1"
+              placeholder="Inserta el peso (en kg)"
+            />
+            {errors.weight && (
+              <div class="errors">
+                <div>{errors.weight}</div>
+              </div>
+            )}
+          </label>
+          <label>
+            Tamaño
+            <input
+              onChange={handleInputChange}
+              value={input.height}
+              name="height"
+              type="number"
+              min="1"
+              placeholder="Inserta el tamaño (cm)"
+            />
+            {errors.height && (
+              <div class="errors">
+                <div>{errors.height}</div>
+              </div>
+            )}
+          </label>
+          <label>
+            Imagen
+            <input
+              onChange={handleInputChange}
+              value={input.image}
+              name="image"
+              type="url"
+              placeholder="Copia la url de la imagen..."
+            />
+            {errors.image && (
+              <div class="errors">
+                <div>{errors.image}</div>
+              </div>
+            )}
+          </label>
+
+          <label>Tipos:</label>
+          <select
+            value={input.typeA}
+            name="typeA"
+            onChange={handleInputChange}
+            required
+          >
+            <option value="typeA">Primer tipo</option>
+            {stateTypes.map((x) => (
+              <option value={x.name} key={x.id}>
+                {x.name}
+              </option>
+            ))}
+          </select>
+          <select value={input.typeB} name="typeB" onChange={handleInputChange}>
+            <option value="StypeB">Segundo tipo</option>
+            {stateTypes.map((e) => (
+              <option value={e.name} key={e.id}>
+                {e.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button class="btn-create" type="submit">
+          Crear
+        </button>
+      </form>
+
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
---
+++ client/src/components/Create/validateForm.js
@@ -0,0 +1,83 @@
+export function validate(input) {
+  let errors = {};
+  const urlOK = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png|svg)/gi;
+  const numberExpresion = /^-?\d*(\.\d+)?$/;
+  //   El método search() y test() ejecutan una búsqueda que encaje entre una expresión regular y el objeto desde el que se llama.
+
+  //Name:
+
+  if (!input.name) {
+    errors.name = "Debes ingresar un nombre para el Pokemon.";
+  } else if (input.name.search(/^[a-zA-Zñáéíóúü]*$/)) {
+    //Valido el input con expresion regular.
+    errors.name =
+      "El nombre no puede contener numeros o caracteres especiales.";
+  }
+  if (input.name.length > 23) {
+    errors.name = "El nombre no puede exceder los 23 caracteres";
+  }
+
+  //Image:
+
+  if (input.image && !urlOK.test(input.image)) {
+    errors.image = "Debes ingresar una URL valida";
+  }
+
+  //Life
+
+  if (input.life < 1 || input.life > 100) {
+    errors.life = "El valor de la vida no puede ser menor a 1 ni mayor a 100.";
+  } else if (input.life.search(numberExpresion)) {
+    //Valido el input con expresion regular.
+    errors.life = "El valor ingresado debe ser solo numero. ¡Intenta de nuevo!";
+  }
+
+  //Attack
+
+  if (input.attack < 1 || input.attack > 350) {
+    errors.attack = "El poder de Ataque no puede ser menor a 1 ni mayor a 350.";
+  } else if (input.attack.search(numberExpresion)) {
+    errors.attack =
+      "El valor ingresado debe ser solo numero. ¡Intenta de nuevo!";
+  }
+
+  //Defense
+
+  if (input.defense < 1 || input.defense > 350) {
+    errors.defense = "La defensa no puede ser menor a 1 ni mayor a 350.";
+  } else if (input.defense.search(numberExpresion)) {
+    errors.defense =
+      "El valor ingresado debe ser solo numero. ¡Intenta de nuevo!";
+  }
+
+  //Speed:
+
+  if (input.speed < 1 || input.speed > 100) {
+    errors.speed = "La velocidad no puede ser menor a 1 ni mayor a 100.";
+  } else if (input.speed.search(numberExpresion)) {
+    errors.speed =
+      "El valor ingresado debe ser solo numero. ¡Intenta de nuevo!";
+  }
+
+  //Height
+
+  if (input.height < 1 || input.height > 50) {
+    errors.height = "El valor no puede ser menor a 1 ni mayor a 50 ";
+  } else if (input.height.search(numberExpresion)) {
+    //Valido el input con expresion regular.
+    errors.height =
+      "El valor ingresado debe ser solo numero. ¡Intenta de nuevo!";
+  }
+
+  //Weight
+
+  if (input.weight < 0 || input.weight > 1000) {
+    errors.weight = "El valor no puede ser menor a 1 ni mayor a 1000 ";
+  } else if (input.weight.search(numberExpresion)) {
+    //Valido el input con expresion regular.
+    errors.weight =
+      "El valor ingresado debe ser solo numero. ¡Intenta de nuevo!";
+  }
+
+  return errors;
+}
