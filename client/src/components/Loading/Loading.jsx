7eccb2c5596e5a96dc5745849e26b4e669422ed9
The Francisco Molina <fdm.molina@gmail.com>
2022-03-27 18:13:04 -0300

    Loading con estilos mejorados

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -5,7 +5,6 @@ export function Loading() {
   return (
     <div class="loader-container">
       <img class="loading-img" src={imageLoading} alt="loaging" />
-      <h3 class="text-loading">Cargando Pokemones...</h3>
     </div>
   );
 }
